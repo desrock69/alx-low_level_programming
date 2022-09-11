0e5443cf5a2713410529a3c91ea7962dd393e1ac
desrock69 <desrock69@gmail.com>
2022-09-10 20:14:37 -0700

    learning

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
 /**
+ * main - Prints out a string to stdout.
  *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-          	return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
