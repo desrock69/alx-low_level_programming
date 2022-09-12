912aa6adf4436b236088a6efbcb43d0c9b0b57a9
desrock69 <desrock69@gmail.com>
2022-09-12 16:57:46 -0700

    all

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - this is putchar
+ *
+ * Description:program runs
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char chr;
+
+	for (chr =  'a'; chr <= 'z' chr++)
+	{
+		chr = tolower(chr);
+		putchar(chr);
+	}
+	putchar('\n');
+
+	return (0);
