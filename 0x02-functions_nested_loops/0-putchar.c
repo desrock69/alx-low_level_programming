bf5f31aa1d6fcfe3a802e9f52d5ed082c4728f18
desrock69 <desrock69@gmail.com>
2022-09-13 09:37:26 -0700

    here

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -my entry point
+ *
+ * Description:print ch
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	while (char = ch;)
+
+	putchar(ch);
+
+	putchar('\n');
+}
