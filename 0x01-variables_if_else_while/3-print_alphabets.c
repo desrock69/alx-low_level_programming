db1c8207858a6b8775fe4880b66fa1f54f465eee
desrock69 <desrock69@gmail.com>
2022-09-12 18:30:50 -0700

    All

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,14 +11,16 @@ int main(void)
 {
 	char chr;
 	char j;
+	char lower_case_char;
+	char upper_case_char;
 
 	for (chr = 'a'; chr <= 'z'; chr++)
-		chr = tolower(chr);
-	putchar(chr);
+		lower_case_char = tolower(chr);
+	putchar(lower_case_char);
 
 	for (j = 'a'; j <= 'z'; j++)
-		j = toupper(j);
-			putchar(j);
+		upper_case_char = toupper(j);
+			putchar(upper_case_char);
 
 			putchar('\n');
 			return (0);
