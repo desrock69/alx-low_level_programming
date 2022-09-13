992d767141454f89e02770c1310f0c27cf5a6ac7
desrock69 <desrock69@gmail.com>
2022-09-12 19:57:09 -0700

    all

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - my mail
+ *
+ * Description:overall
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+
+	putchar(ch + '0');
+
+	putchar('\n');
+
+	return (0);
+}
