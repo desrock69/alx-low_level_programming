42c357d8bad0bb3b1740f472a09be7a88330d4f1
desrock69 <desrock69@gmail.com>
2022-09-12 18:20:06 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - print out letters
+ *
+ * Description: this is a description
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char chr;
+	char j;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+		chr = tolower(chr);
+	putchar(chr);
+
+	for (j = 'a'; j <= 'z'; j++)
+		j = toupper(chr);
+			putchar(j);
+
+			putchar('\n');
+			return (0);
+}
