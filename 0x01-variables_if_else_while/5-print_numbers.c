e89a901420b6f0bf2dcd3f41ab1aaeef8f6860d1
desrock69 <desrock69@gmail.com>
2022-09-12 19:17:19 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - my apple
+ *
+ * Description: lollipop
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+
+		printf("%d", i);
+
+	putchar('\n');
+
+	return (0);
+}
