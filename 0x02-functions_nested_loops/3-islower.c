ada3e3606df1c550c34801403f0b7ebffcb6c315
desrock69 <desrock69@gmail.com>
2022-09-14 18:26:26 -0700

    lola

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - a fxn that checks for lower case character
+ * @: single letter input
+ * Return: 1 if int c is lowercase, 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
