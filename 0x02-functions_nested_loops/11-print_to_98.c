83774bb06f68fb2f085f87daf17942b6c8acb485
desrock69 <desrock69@gmail.com>
2022-09-15 05:50:06 -0700

    hjj

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  * print_to_98 - a function that prints all natural numbers
- * user input's number prints to 98, regardless < 98 or > 98
+ * Description: user input's number prints to 98, regardless < 98 or > 98
  * @n: number input
  * Return: Always 0 (Success)
  */
