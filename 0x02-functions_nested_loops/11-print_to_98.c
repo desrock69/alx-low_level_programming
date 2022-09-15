dca5165b6fbaed69cceab53306cf59a1f822ec7d
desrock69 <desrock69@gmail.com>
2022-09-15 05:51:53 -0700

    kkfd

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,11 +2,11 @@
 #include "main.h"
 /**
  * print_to_98 - a function that prints all natural numbers
- * Description: user input's number prints to 98, regardless < 98 or > 98
+ * user input's number prints to 98, regardless < 98 or > 98
  * @n: number input
  * Return: Always 0 (Success)
  */
-void print_to_98(int n)
+void print_to_98(char n)
 {
 	if (n < 98)
 	{
