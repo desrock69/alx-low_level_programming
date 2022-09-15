f2580ed905250fcd39edcc7e81c33208104f78ba
desrock69 <desrock69@gmail.com>
2022-09-14 18:35:44 -0700

    lalal

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,6 +2,8 @@
 /**
  * _isaplha - a fxn that checks for alphabetic character
  * @c: single letter input
+ * Description: a lalalala
+ *
  * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
  */
 int _isalpha(int c)
