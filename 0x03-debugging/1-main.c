826ab51dec7af50fba44892890689232e0c04eb0
desrock69 <desrock69@gmail.com>
2022-09-15 21:58:14 -0700

    pult

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+x#include <stdio.h>
 /**
  * main - causes an infinite loop
  * Return: 0
@@ -8,9 +8,9 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
+		i = 0;
 	/**
-	*	i = 0;
-	*
 	*	while (i < 10)
 	*	{
 	*	putchar(i);
